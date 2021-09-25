5ebb60928f3947273e790341fde502326201bf8d
Kjell Maekelberg <maekelbergkjell@gmail.com>
2021-09-25 20:47:41 +0200

    add doubleclick reminder functionality

--- src/App.js
+++ src/App.js
@@ -24,15 +24,16 @@ function App() {
 
     const deleteTask = (id) => {
         setTasks(tasks.filter((task) => task.id !== id))
+    }
 
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
     }
 
     return (
         <div  className="container">
             <Header title="Task Tracker"/>
-
-            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : "No tasks yet..."}
-
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks yet..."}
         </div>
     );
 }
