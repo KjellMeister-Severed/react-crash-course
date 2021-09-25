5bf18b8abd9fbad25bebeda987dc0a2ca58a80f8
Kjell Maekelberg <maekelbergkjell@gmail.com>
2021-09-25 23:18:43 +0200

    Implement "backend" connectivity with initial fetch request.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import AddTask from "./components/AddTask";
 
 function App() {
     const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: "Doctors Appointment",
-            day: 'Feb 5th at 2:30pm',
-            reminder: true
-        }, {
-            id: 2,
-            text: "Meeting at School",
-            day: 'Feb 6th at 1:30pm',
-            reminder: true
-        }, {
-            id: 3,
-            text: "Food Shopping",
-            day: 'Feb 5th at 1:30pm',
-            reminder: false
-        }
     ])
+
+    useEffect(() => {
+        const getTasks = async () => {
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
+        }
+        getTasks().then()
+    }, [])
+
+    const fetchTasks = async () => {
+        const res = await fetch('http://localhost:5000/tasks');
+        return await res.json();
+    }
 
     const addTask = (task) => {
         const id = tasks.length + 1;
