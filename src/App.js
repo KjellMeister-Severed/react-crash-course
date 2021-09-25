0e20f6b1e163ca9faedb4a1365cc2701c324ae6e
Kjell Maekelberg <maekelbergkjell@gmail.com>
2021-09-26 00:50:47 +0200

    Implement routing instead of regular page navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Header from "./components/Header";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Tasks from "./components/Tasks";
 import {useState, useEffect} from "react";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
     const [showAddTask, setShowAddTask] = useState(false)
@@ -70,12 +73,22 @@ function App() {
     }
 
     return (
-        <div className="container">
-            <Header title="Task Tracker" onAdd={toggleAddForm} showAddTask={showAddTask}/>
-            {showAddTask && <AddTask onAdd={addTask}/>}
-            {tasks.length > 0 ?
-                <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks yet..."}
-        </div>
+        <Router>
+            <div className="container">
+                <Header title="Task Tracker" onAdd={toggleAddForm} showAddTask={showAddTask}/>
+                <Route path={'/about' } component={About} />
+                <Route path={'/'} exact render={(props) => (
+                        <>
+                            {showAddTask && <AddTask onAdd={addTask}/>}
+                            {tasks.length > 0 ?
+                            <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks yet..."}
+                        </>
+                    )}
+                />
+                <Footer/>
+            </div>
+        </Router>
+
     );
 }
 
