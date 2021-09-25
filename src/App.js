d2ea31f7def087f9e3cb9ce4cf8670e9d2df9ef4
Kjell Maekelberg <maekelbergkjell@gmail.com>
2021-09-25 23:46:22 +0200

    Implement additional POST, PUT, DELETE requests for saved datastorage.

--- src/App.js
+++ src/App.js
@@ -21,20 +21,49 @@ function App() {
         return await res.json();
     }
 
-    const addTask = (task) => {
-        const id = tasks.length + 1;
-        const newTask = {
-            id, ...task
-        }
-        setTasks([...tasks, newTask])
+    const fetchSingleTask = async (id) => {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`);
+        return await res.json();
     }
 
-    const deleteTask = (id) => {
+    const addTask = async (task) => {
+        const res = await fetch("http://localhost:5000/tasks/", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        });
+
+        const data = await res.json();
+        setTasks([...tasks, data])
+    }
+
+    const deleteTask = async (id) => {
+        await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'DELETE'
+        })
+
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
-    const toggleReminder = (id) => {
-        setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchSingleTask(id);
+        const updTask = {
+            ...taskToToggle,
+            reminder: !taskToToggle.reminder
+        }
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'PUT',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updTask)
+        })
+
+        const data = await res.json();
+
+        setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder} : task))
     }
 
     const toggleAddForm = () => {
